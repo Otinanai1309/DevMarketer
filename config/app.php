87d15eda50592fae7f8df9687a9406c2afe9ac5a
= <=>
2017-08-04 18:36:34 +0300

    setup laratrust with table seeder and populate database

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+          * Third party service Providers
+        */
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -210,6 +215,7 @@ return [
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
